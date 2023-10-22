25bb70a70a8c144be5304db1ae5e1c22078eea2d
Karthikmani6025 <101208393+Karthikmani6025@users.noreply.github.com>
2023-10-22 16:00:03 +0530

    Update AdactinLogin.java

--- src/test/java/org/gt/test/login/AdactinLogin.java
+++ src/test/java/org/gt/test/login/AdactinLogin.java
@@ -2,18 +2,18 @@ package org.gt.test.login;
 
 public class AdactinLogin {
 	public void method() {
-		System.out.println("Bunky");
+		System.out.println("Vini");
 	}
 	public void method1() {
-		System.out.println("Ranji");
+		System.out.println("Karthik");
 	}
 	public void method2() {
-		System.out.println("Karthik");
+		System.out.println("Ranji");
 	}
 	public void method3() {
 		System.out.println("Sri");
 	}
 	public void method4() {
-		System.out.println("Vini");
+		System.out.println("Bunky");
 	}
 }
